34c284ef1cfcc80a55518c8c1331130d665e2363
garydsong <ceouls@gmail.com>
2023-07-29 16:51:10 -0700

    starfield

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,8 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  const currentHour:string = moment.tz("America/Los_Angeles").format('HH');
-  const testHour = 9
+  const currentHour: string = moment.tz("America/Los_Angeles").format('HH');
+  const testHour = 19
 
   return (
     <html lang="en">
@@ -27,17 +27,30 @@ export default function RootLayout({
       </header>
       <body className={
         +testHour >= 19 || +testHour <= 6
-        ? "bg-gradient-to-r from-black via-blue-900 to-black"
-        : "bg-gradient-to-r from-blue-400 via-blue-200 to-blue-400"
-        }>
+          ? "bg-gradient-to-r from-black via-blue-900 to-black"
+          : "bg-gradient-to-r from-blue-400 via-blue-200 to-blue-400"
+      }>
         <div className="flex justify-center items-center w-full h-full">
           <Weather></Weather>
         </div>
         <div className={
           +testHour >= 19 || +testHour <= 6
-          ? "absolute top-50 right-50 w-72 h-72 bg-white rounded-full opacity-55 filter blur-2xl animate-blob"
-          : "absolute top-50 right-50 w-72 h-72 bg-yellow-300 rounded-full opacity-55 filter blur-2xl animate-blob"
-          }></div>
+            ? "absolute top-50 right-50 w-72 h-72 bg-white rounded-full opacity-55 filter blur-2xl animate-blob"
+            : "absolute top-50 right-50 w-72 h-72 bg-yellow-300 rounded-full opacity-55 filter blur-2xl animate-blob"
+        }></div>
+
+        {+testHour >= 19 || +testHour <= 6
+          ?
+          (<div id="space">
+            <div className="stars"></div>
+            <div className="stars"></div>
+            <div className="stars"></div>
+            <div className="stars"></div>
+            <div className="stars"></div>
+          </div>)
+          : (<></>)
+        }
+
       </body>
     </html>
   )
