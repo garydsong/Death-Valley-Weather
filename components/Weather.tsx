87bbaed12a131665e339ce0ce168b2d33d989dfe
garydsong <ceouls@gmail.com>
2023-07-29 16:47:10 -0700

    dynamic styling based on time

--- components/Weather.tsx
+++ components/Weather.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 const apiKey = "e1717085edb84ced802203246232807"
 const url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=Death Valley&days=1&aqi=no&alerts=no`
+const moment = require('moment-timezone');
 
 export default async function Weather() {
     async function fetchWeather() {
@@ -10,13 +11,19 @@ export default async function Weather() {
         return result
     }
 
-    const weather:object = await fetchWeather()
+    const currentHour: string = moment.tz("America/Los_Angeles").format('HH');
+
+    const weather: object = await fetchWeather()
     console.log(weather)
+
     return (
         <div className="absolute z-50 font-dm-sans">
-            <div className="text-6xl font-extrabold">{weather.location.name}</div>
-            <div className="font-bold ">{weather.location.region.toUpperCase()}</div>
-            <div>{weather.current.condition.text}</div>
+            <div className="text-6xl font-extrabold">{weather.location.name}
+            </div>
+            <div className="font-bold">{weather.location.region.toUpperCase()}</div>
+            <div>
+                {weather.current.condition.text}
+            </div>
             <div>{weather.current.temp_f}° F</div>
             <div>{weather.current.temp_c}° C</div>
         </div>
