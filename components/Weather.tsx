7178c490acce45fe376d98ae12b076d0a65117c3
garydsong <ceouls@gmail.com>
2023-07-30 10:12:34 -0700

    useEffect instead of async component for weather componenet.

--- components/Weather.tsx
+++ components/Weather.tsx
@@ -1,20 +1,29 @@
-
-import React from 'react'
+"use client"
+import React, {useState, useEffect} from 'react'
 const apiKey = "e1717085edb84ced802203246232807"
 const url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=Death Valley&days=1&aqi=no&alerts=no`
 const moment = require('moment-timezone');
 
-export default async function Weather() {
+export default function Weather() {
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [weather, setWeatherData] = useState(null)
+    const [fcSwitch, setFCSwitch] = useState(true) // starts at true to default to F
+    // async function fetchWeather() {
+    //     const response = await fetch(url)
+    //     const result = await response.json()
 
-    async function fetchWeather() {
-        const response = await fetch(url)
-        const result = await response.json()
-
-        return result
-    }
+    //     return result
+    // }
 
 
-    const weather: object = await fetchWeather()
+    // const weather: object = await fetchWeather()\
+
+    useEffect(() => {
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setWeatherData(data))
+        .then(() => setIsLoaded(true))
+    }, [])
 
     const currentHour: string = moment.tz("America/Tijuana").format('HH');
 
@@ -22,13 +31,14 @@ export default async function Weather() {
 
     const currentTime: string = moment.tz("America/Tijuana").format('HH:mm');
 
-    let fcSwitch = true;
+    // let fcSwitch = true;
 
     console.log(weather)
 
-
-    console.log(fcSwitch)
-    return (
+    // const setFC = () => {
+    //     fcSwitch ? fcSwitch = false : fcSwitch = true
+    // }
+    return isLoaded && (
         <div className="absolute z-50 font-dm-sans z-10">
             <div className="flex flex-col items-center">
 
@@ -37,12 +47,12 @@ export default async function Weather() {
                         ? (<div className="flex">{weather.current.temp_f}°
                         <div>F</div>
                         /
-                        <div>C</div>
+                        <div className="cursor-pointer hover:text-gray-400 duration-1000" onClick={() => setFCSwitch(false)}>C</div>
                         </div>)
                         : (<div className="flex">{weather.current.temp_c}°
-                        <div>F</div>
-                        /
                         <div>C</div>
+                        /
+                        <div className="cursor-pointer hover:text-gray-400 duration-1000" onClick={() => setFCSwitch(true)}>F</div>
                         </div>)
                     }
                 </div>
